d37f14fba52eba0daa33feea709f159ee4df4e7d
Adam Grant <adammgrant@gmail.com>
2017-11-14 13:51:29 -0800

    add handling for default select value

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -24,7 +24,7 @@ class ListBooks extends Component {
                                         backgroundImage: `url(${book.imageLinks.thumbnail})`
                         }}></div>
                         <div className="book-shelf-changer">
-                          <select onChange={(event) => onUpdateBook(book, event.target.value)}>
+                          <select value={book.shelf} onChange={(event) => onUpdateBook(book, event.target.value)}>
                             <option value="none" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
